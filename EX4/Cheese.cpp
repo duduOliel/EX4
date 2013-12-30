d96d5bb77d8f9b99cac8ea8a4b69c3fb58c8be27
MuppetJumper <MuppetJumper@gmail.com>
2013-12-30 19:44:57 +0200

    Added Cheese Meta-Stub

---
+++ EX4/Cheese.cpp
@@ -0,0 +1,11 @@
+#include "Cheese.h"
+
+
+Cheese::Cheese(void) 
+{
+}
+
+
+Cheese::~Cheese(void)
+{
+}
---
+++ EX4/Cheese.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "dairyproduct.h"
+class Cheese : public DairyProduct
+{
+public:
+	Cheese(void);
+	virtual ~Cheese(void);
+};
+
