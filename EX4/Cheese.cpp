0e1ab2694c9c308ea6ad872ff490865450c3290d
Yuval Cohen <muppetjumper@gmail.com>
2014-01-01 19:28:09 +0200

    Finished Cheese implementation

--- EX4/Cheese.cpp
+++ EX4/Cheese.cpp
@@ -1,11 +1,37 @@
 #include "Cheese.h"
 
 
-Cheese::Cheese(void) 
+const string Cheese::ADDINGS_RANGE_ERROR = "Addings for cheese must be positive";
+
+
+Cheese::Cheese(string name, int colorsInBox, int fatPrecentage, Location location, Exposure exposure, int addingsQuantity)
+	:DairyProduct(name,DairyProduct::CHEESE,colorsInBox,fatPrecentage,location,exposure)
 {
+	setAddingsQuantity(addingsQuantity);
 }
 
 
 Cheese::~Cheese(void)
 {
 }
+
+int Cheese::calcPrice( int factor )
+{
+	return (DairyProduct::calcPrice(factor) + getAddingsQuantity());
+}
+
+void Cheese::setAddingsQuantity( int addingsQuantity )
+{
+	if (addingsQuantity < MIN_ADDINGS)
+		throw std::range_error(ADDINGS_RANGE_ERROR);
+	else
+		_addingsQuantity = addingsQuantity;
+}
+
+ostream& operator<<(ostream& out, const Cheese& obj)
+{
+	const DairyProduct& base = (DairyProduct)obj;
+	out << base << " (" << obj.getAddingsQuantity() << ")";
+	return out;
+}
+
--- EX4/Cheese.h
+++ EX4/Cheese.h
@@ -2,8 +2,31 @@
 #include "dairyproduct.h"
 class Cheese : public DairyProduct
 {
+private:
+
+	// Data Members
+	int _addingsQuantity;
+
+	// Static Consts
+	static const int MIN_ADDINGS = 0;
+
+	// Static Error Messages
+	static const string ADDINGS_RANGE_ERROR;
+
 public:
-	Cheese(void);
+
+	// Ctor Dtor
+	Cheese(string name, int colorsInBox, int fatPrecentage, Location location, Exposure exposure, int addingsQuantity);
 	virtual ~Cheese(void);
+
+	// Accessors 
+	int getAddingsQuantity() const { return _addingsQuantity; }
+	void setAddingsQuantity(int addingsQuantity);
+
+	// Calculation Method
+	int calcPrice(int factor);
+
+	// Print Method
+	friend ostream& operator<<(ostream& out, const Cheese& obj);
 };
 
