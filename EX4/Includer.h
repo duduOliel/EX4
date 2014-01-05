b038b9a0b5f365d723ed8134e6a40ce2e88c479e
Yuval Cohen <muppetjumper@gmail.com>
2014-01-05 22:29:43 +0200

    Includer.h to beautify main.cpp includes

---
+++ EX4/Includer.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include "Product.h"
+#include "AgriculturalProduct.h"
+#include "IllegalArgumentException.h"
+#include "Package.h"
+#include "Store.h"
+#include "Fruit.h"
+#include "Vegetable.h"
+#include "Cheese.h"
+#include "EnhancedDairyProduct.h"
+
+using namespace std;
