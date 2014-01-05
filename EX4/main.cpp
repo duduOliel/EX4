6bced2d4edcad0dda9dbdc65b53c30bad425b9d1
Yuval Cohen <muppetjumper@gmail.com>
2014-01-05 22:24:48 +0200

    Added products quantity for better testing Improved documentation (copy paste errors...)

--- EX4/main.cpp
+++ EX4/main.cpp
@@ -15,6 +15,7 @@ void main()
 	const int APPLE_RIPE = 2;
 	const int APPLE_PROVIDORS = 3;
 	const int APPLE_SHELF = 5;
+	const int APPLE_QUANTITY = 10;
 
 	// Carrot metadata
 	const int CARROT_VITAMINS = 4;
@@ -22,6 +23,8 @@ void main()
 	const int CARROT_RIPE = 4;
 	const int CARROT_PROVIDORS = 2;
 	const int CARROT_SHELF = 4;
+	const int CARROT_QUANTITY = 15;
+
 
 	// Cheese metadata
 	const string CHEESE_NAME = "Yuval's gaudy";
@@ -29,6 +32,7 @@ void main()
 	const int CHEESE_FAT_PERCENT = 30;
 	const int CHEESE_SHELF = 1;
 	const int CHEESE_ADDINGS = 1;
+	const int CHEESE_QUANTITY = 7;
 
 	// Icecream metadata
 	const string ICECREAM_NAME = "Vanila and strawberry ice cream";
@@ -36,6 +40,7 @@ void main()
 	const int ICECREAM_FAT_PERCENT = 60;
 	const int ICECREAM_SHELF = 4;
 	const int ICECREAM_COMPONENTS_COUNT = 2;
+	const int ICECREAM_QUANTITY = 4;
 	string ICECREAM_COMPONENTS[ICECREAM_COMPONENTS_COUNT] = {"VANILA STICK","STRAWBERRY SURBET"};
 
 	// Almost healthy Package metadata
@@ -43,6 +48,7 @@ void main()
 	string PACKAGE_PRODUCTS[PACKAGE_PROD_COUNT] = {APPLE_NAME,CARROT_NAME,CHEESE_NAME,ICECREAM_NAME};
 	const int PACKAGE_COLORS = 3;
 	const int PACKAGE_SHELF = 1;
+	const int PACKAGE_QUANTITY = 1;
 	
 	// Printing empty Shop details
 	Store superShop(STORE_NAME,PUBLICITY_FACTOR);
@@ -53,6 +59,7 @@ void main()
 	appleLocation.setAisle(FRUIT_AISLE);
 	appleLocation.setShelf(APPLE_SHELF);
 	Fruit apple (APPLE_SUGAR,APPLE_NAME,APPLE_RIPE,APPLE_PROVIDORS, appleLocation,Product::MEDIUM);
+	apple.setQuantity(CARROT_QUANTITY);
 
 	// Printing apple details
 	cout << apple;
@@ -66,6 +73,7 @@ void main()
 	carrotLocation.setAisle(VEG_AISLE);
 	carrotLocation.setShelf(CARROT_SHELF);
 	Vegetable carrot(CARROT_VITAMINS,CARROT_NAME,CARROT_RIPE,CARROT_PROVIDORS,carrotLocation,Product::MAX);
+	carrot.setQuantity(CARROT_QUANTITY);
 
 	// Printing carrot details
 	cout << carrot;
@@ -79,6 +87,7 @@ void main()
 	cheeseLocation.setAisle(DAIRY_AISLE);
 	cheeseLocation.setShelf(CHEESE_SHELF);
 	Cheese cheese(CHEESE_NAME,CHEESE_COLORS,CHEESE_FAT_PERCENT,cheeseLocation,Product::MEDIUM,CHEESE_ADDINGS);
+	cheese.setQuantity(CHEESE_QUANTITY);
 
 	// Printing cheese details
 	cout << cheese;
@@ -92,11 +101,12 @@ void main()
 	icecreamLoc.setAisle(DAIRY_AISLE);
 	icecreamLoc.setShelf(ICECREAM_SHELF);
 	EnhancedDairyProduct icecream(ICECREAM_NAME,ICECREAM_COLORS,ICECREAM_FAT_PERCENT,icecreamLoc,Product::MAX,ICECREAM_COMPONENTS,ICECREAM_COMPONENTS_COUNT);
+	icecream.setQuantity(ICECREAM_QUANTITY);
 
 	// Printing icecream details
 	cout << icecream;
 
-	// Adding cheese to the store
+	// Adding icecream  to the store
 	superShop.addProduct(icecream);
 	cout << superShop.sumAllPrices();
 
@@ -105,6 +115,7 @@ void main()
 	pckgLoc.setAisle(DAIRY_AISLE);
 	pckgLoc.setShelf(PACKAGE_SHELF);
 	Package almostHealthyPackage(PACKAGE_PRODUCTS,PACKAGE_PROD_COUNT,PACKAGE_COLORS,pckgLoc,Product::HIDDEN); // Hidden because this probably taste like shit
+	almostHealthyPackage.setQuantity(PACKAGE_QUANTITY);
 
 	// Printing package details
 	cout << pckgLoc;
